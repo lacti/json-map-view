72a563a7efb040e1d1e934c1ebc2dcc1edbf9de4
jaeyoung.choi <lactrious@gmail.com>
2019-09-18 00:26:32 +0900

    Initial commit

---
+++ precache-manifest.0ed7c5eb2f12104a56ee3b1976947b16.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9605dfecfa6a61d68f61500195854a03",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa2f94151c4a2586963e",
+    "url": "/static/css/main.72dbf3f6.chunk.css"
+  },
+  {
+    "revision": "233eba391b3ff3f284dd",
+    "url": "/static/js/2.ee0d0433.chunk.js"
+  },
+  {
+    "revision": "fa2f94151c4a2586963e",
+    "url": "/static/js/main.d133f433.chunk.js"
+  },
+  {
+    "revision": "f26af403c5b113f303ba",
+    "url": "/static/js/runtime~main.7764714f.js"
+  }
+]);
