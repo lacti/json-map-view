3b1adfe80ca721d69453570d2b9502689cfc55d9
jaeyoung.choi <lactrious@gmail.com>
2019-11-21 16:19:25 +0900

    Support multiple keywords query

--- src/utils/query.ts
+++ src/utils/query.ts
@@ -1,36 +1,55 @@
 import { IMapNode } from "../models/node";
 
-export const applyQuery = (root: IMapNode, query: string | null) => {
-  if (!query) {
+export const applyQuery = (root: IMapNode, fullQuery: string | null) => {
+  if (!fullQuery) {
     return root;
   }
-  const effectiveQuery = query.toLowerCase();
-  const filterRecursively = (node: IMapNode, level: number) => {
+  const filterRecursively = (
+    keyword: string,
+    node: IMapNode,
+    level: number,
+    parentSelected: boolean
+  ) => {
     const filteredNode: IMapNode = {};
     const filteredKeys = Object.keys(node).filter(key =>
-      key.toLowerCase().includes(effectiveQuery)
+      key.toLowerCase().includes(keyword)
     );
     const targetKeys =
       filteredKeys.length > 0 ? filteredKeys : Object.keys(node);
     for (const key of targetKeys) {
       const value = node[key];
-      if (key.toLowerCase() === effectiveQuery) {
+      if (key.toLowerCase() === keyword) {
         filteredNode[key] = value;
         continue;
       }
 
       if (typeof value === "string") {
-        if (key.toLowerCase().includes(effectiveQuery)) {
+        if (key.toLowerCase().includes(keyword)) {
           filteredNode[key] = value;
         }
       } else {
-        const filteredChild = filterRecursively(value, level + 1);
+        const filteredChild = filterRecursively(
+          keyword,
+          value,
+          level + 1,
+          filteredKeys.length > 0
+        );
         if (Object.keys(filteredChild).length > 0) {
           filteredNode[key] = filteredChild;
         }
       }
     }
+    if (parentSelected && Object.keys(filteredNode).length === 0) {
+      return node;
+    }
     return filteredNode;
   };
-  return filterRecursively(root, 0);
+  return fullQuery
+    .toLowerCase()
+    .split(/\s+/)
+    .reduce(
+      (intermediate, keyword) =>
+        filterRecursively(keyword, intermediate, 0, false),
+      root
+    );
 };
